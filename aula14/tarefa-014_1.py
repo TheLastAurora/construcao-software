2ce730c022e7b116785a1db5d41421cd91122683
Vitor L <vitoribeiro0024@gmail.com>
2023-05-26 20:41:16 -0300

    new file:   aula12/tarefa-010/.gitignore 	new file:   aula12/tarefa-010/.scalafmt.conf 	modified:   aula14/tarefa-014_1.py

--- aula14/tarefa-014_1.py
+++ aula14/tarefa-014_1.py
@@ -1,112 +1,83 @@
 import random
 import time
 
-class SortingAlgorithm:
-    @staticmethod
-    def merge_sort_iterative(arr):
-        if len(arr) <= 1:
-            return arr
-
-        def merge(left, right):
-            merged = []
-            left_index = right_index = 0
-
-            while left_index < len(left) and right_index < len(right):
-                if left[left_index] < right[right_index]:
-                    merged.append(left[left_index])
-                    left_index += 1
-                else:
-                    merged.append(right[right_index])
-                    right_index += 1
-
-            merged.extend(left[left_index:])
-            merged.extend(right[right_index:])
-            return merged
-
-        merged_arr = [[num] for num in arr]
-
-        while len(merged_arr) > 1:
-            next_level = []
-            for i in range(0, len(merged_arr), 2):
-                if i + 1 < len(merged_arr):
-                    merged = merge(merged_arr[i], merged_arr[i + 1])
-                else:
-                    merged = merged_arr[i]
-                next_level.append(merged)
-            merged_arr = next_level
-
-        return merged_arr[0]
-
-    @staticmethod
-    def merge_sort_recursive(arr):
-        if len(arr) <= 1:
-            return arr
-
-        def merge(left, right):
-            if not left:
-                return right
-            if not right:
-                return left
-
-            if left[0] < right[0]:
-                return [left[0]] + merge(left[1:], right)
-            return [right[0]] + merge(left, right[1:])
-
-        mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
-
-        left = SortingAlgorithm.merge_sort_recursive(left)
-        right = SortingAlgorithm.merge_sort_recursive(right)
-
-        return merge(left, right)
-
-    @staticmethod
-    def quick_sort_iterative(arr):
-        if len(arr) <= 1:
-            return arr
-
-        stack = [(0, len(arr) - 1)]
-
-        while stack:
-            start, end = stack.pop()
-            pivot_index = SortingAlgorithm.partition(arr, start, end)
-
-            if pivot_index - 1 > start:
-                stack.append((start, pivot_index - 1))
-            if pivot_index + 1 < end:
-                stack.append((pivot_index + 1, end))
-
+def merge_sort(arr):
+    if len(arr) <= 1:
         return arr
+    
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    
+    left = merge_sort(left)
+    right = merge_sort(right)
+    
+    return merge(left, right)
 
-    @staticmethod
-    def quick_sort_recursive(arr):
-        if len(arr) <= 1:
-            return arr
+def merge(left, right):
+    merged = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+    
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+    
+    return merged
 
-        pivot_index = SortingAlgorithm.partition(arr, 0, len(arr) - 1)
-        left = SortingAlgorithm.quick_sort_recursive(arr[:pivot_index])
-        right = SortingAlgorithm.quick_sort_recursive(arr[pivot_index + 1:])
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[len(arr) // 2]
+    smaller = [x for x in arr if x < pivot]
+    equal = [x for x in arr if x == pivot]
+    larger = [x for x in arr if x > pivot]
+    
+    return quick_sort(smaller) + equal + quick_sort(larger)
 
-        return left + [arr[pivot_index]] + right
+class TesteOrdenacao:
+    def __init__(self):
+        self.tamanhos = [100, 1000, 10000]
+    
+    def gerar_vetor(self, tamanho):
+        return [random.randint(1, 1000) for _ in range(tamanho)]
+    
+    def executar_teste(self, algoritmo, tamanho):
+        vetor = self.gerar_vetor(tamanho)
+        
+        inicio = time.time()
+        algoritmo(vetor)
+        fim = time.time()
+        
+        tempo_execucao = fim - inicio
+        print(f"Tempo de execução do algoritmo {algoritmo.__name__} para vetor de tamanho {tamanho}: {tempo_execucao:.6f} segundos")
+    
+    def executar_testes(self):
+        for tamanho in self.tamanhos:
+            vetor = self.gerar_vetor(tamanho)
+            print(f"Vetor original: {vetor}")
+            
+            print("Executando MergeSort:")
+            self.executar_teste(merge_sort, tamanho)
+            print()
+            
+            print("Executando QuickSort:")
+            self.executar_teste(quick_sort, tamanho)
+            print()
+            print("=" * 40)
+            print()
 
-    @staticmethod
-    def partition(arr, low, high):
-        pivot = arr[high]
-        i = low - 1
-
-        for j in range(low, high):
-            if arr[j] < pivot:
-                i += 1
-                arr[i], arr[j] = arr[j], arr[i]
-
-        arr[i + 1], arr[high] = arr[high], arr[i + 1]
-        return i + 1
-
-
-class TestSortingAlgorithms:
-    @staticmethod
-    def generate_random_array(size):
-        return [random.randint(1, 1000) for _ in range(size)]
-
-    @staticmethod
+teste = TesteOrdenacao()
+teste.executar_testes()
